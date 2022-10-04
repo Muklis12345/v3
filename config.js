f8783f39fb6e6556171912c497a029e878077878
Muklis12345 <114465542+Muklis12345@users.noreply.github.com>
2022-10-04 14:46:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -49,28 +49,28 @@ let wibh = moment.tz('Asia/Jakarta').format('HH')
 
 // kalo ga punya ketik "-" atau biarin aja biar ada creditsnya :v
 
-global.sig = 'https://instagram.com/_ctzhid' // ig
-global.sgh = 'https://github.com/WH-MODS-BOT/' // github
-global.sgc = 'https://chat.whatsapp.com/FQkbyTEfvES3D363uxMEFOI' // group whatsapp
-global.sdc = 'https://www.discord.com'//discord
-global.snh = 'https://www.youtube.com/channel/UCMx4e8anOq_Olt2nMSv0Cow' // my youtube channel
-global.sfb = 'https://www.facebook.com/' // facebook
-global.syt = 'https://www.youtube.com/' // my youtube channel "kalau ada yt ke 2 tarok link nya disini"
+global.sig = '1' // ig
+global.sgh = '2' // github
+global.sgc = '3' // group whatsapp
+global.sdc = '4'//discord
+global.snh = '5' // my youtube channel
+global.sfb = '6' // facebook
+global.syt = '7' // my youtube channel "kalau ada yt ke 2 tarok link nya disini"
 
 /*============== PAYMENT ==============*/
-global.pdana = '08XXXXXXXXXX' //dana
-global.povo = '08XXXXXXXXXX' //ovo
-global.pgopay = '08XXXXXXXXXX' //gopay
+global.pdana = '085896661014' //dana
+global.povo = '0XXXXXXXXXXX' //ovo
+global.pgopay = '085838571629' //gopay
 global.plinkaja = '08XXXXXXXXXX' //link aja
-global.ppulsa = '08XXXXXXXXXX' //telkomsel
-global.ppulsa2 = '08XXXXXXXXXX' // kalau ada kartu lain isi aja
-global.psaweria = 'https://saweria.com/mrw4h1d'
+global.ppulsa = '085838571629' //indosat
+global.ppulsa2 = '085768734014' //indosat
+global.psaweria = 'BLM ADA!!!'
 
 /*============== NOMOR ==============*/
-global.nomorbot = '62XXXXXXXXXXX'
-global.nomorown = '6282127487538'
-global.namebot = 'WH-MODS-BOT-V1'
-global.nameown = 'WH MODS DEV'
+global.nomorbot = '6285838571629'
+global.nomorown = '6285838142263'
+global.namebot = '𝐊𝐋𝐈𝐙-𝐌𝐃'
+global.nameown = '𝐊𝐋𝐈𝐒 𝐆𝐍𝐓𝐍𝐆'
 
 /*============== Github Thanks To ==============*/
 global.namagithub = 'Nama Github Belum Diisi' // nama github lu
@@ -85,7 +85,7 @@ global.lolkey = 'e54205a4ca2caa368cc067bb' // PUNYA WAHID  8e66d0934cf741bfd2182
 
 /*============== STAFF ==============*/
 global.owner = [
-  ['6282127487538', 'WH MODS DEV', true],
+  ['6285838142263', '𝐊𝐋𝐈𝐙 𝐆𝐍𝐓𝐍𝐆', true],
   ['-', 'ISI KALAU ADA', true],
   ['-', 'ISI KALAU ADA', true],
   ['-', 'ISI KALAU ADA', true],
@@ -138,14 +138,14 @@ global.APIKeys = { // APIKey Here
 // Kata APIKEY itu isi dengan apikey lu sendiri, dengan beli ke website itu
 
 /*============== WATERMARK ==============*/
-global.wm = '                「 WH-MODS-BOT-V1 」' //Main Watermark
-global.wm2 = '꒷︶꒷꒥꒷ ‧₊˚ ꒰ฅ˘WH-MODS-BOT-V1 ˘ฅ ꒱ ‧₊˚꒷︶꒷꒥꒷'
-global.wm3 = '⫹⫺ WH-MODS-BOT-V1 '
+global.wm = '                「 𝐊𝐋𝐈𝐙-𝐁𝐎𝐓-𝐌𝐃 」' //Main Watermark
+global.wm2 = '𝐊𝐋𝐈𝐙-𝐁𝐎𝐓-𝐌𝐃'
+global.wm3 = '𝐊𝐋𝐈𝐙-𝐁𝐎𝐓-𝐌𝐃 '
 global.botdate = `⫹⫺ DATE: ${week} ${date}\n⫹⫺ 𝗧𝗶𝗺𝗲: ${wktuwib}`
 global.bottime = `T I M E : ${wktuwib}`
-global.titlebot = `⫹⫺ WhatsApp Bot | By WH-MODS-DEV`
+global.titlebot = `⫹⫺ WhatsApp Bot | By 𝐊𝐋𝐈𝐙-𝐁𝐎𝐓-𝐌𝐃`
 global.packname = '☂︎ ᴄʀᴇᴀᴛᴇᴅ ʙʏ'
-global.author = '                「 WH-MODS-BOT-V1  あ」'
+global.author = '                「 𝐊𝐋𝐈𝐙-𝐁𝐎𝐓-𝐌𝐃  あ」'
 
 /*Fake*/
 global.fsizedoc = '1'.repeat(10)
@@ -636,8 +636,8 @@ global.htjava = '⫹⫺'    //hiasan Doang :v
 global.hsquere = ['⛶','❏','⫹⫺']
 
 /*============== STICKER WM ==============*/
-global.stickpack = 'WH-MODS-BOT-V1'
-global.stickauth = `By ©WH-MODS-DEV`
+global.stickpack = '𝐊𝐋𝐈𝐙-𝐁𝐎𝐓-𝐌𝐃'
+global.stickauth = `𝐁𝐘 ©𝐊𝐋𝐈𝐙-𝐁𝐎𝐓-𝐌𝐃`
 
 global.multiplier = 38 // The higher, The harder levelup
 
