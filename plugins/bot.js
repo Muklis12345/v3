dde3db07ce05469ae97230312895b7510e19bac3
Muklis12345 <114465542+Muklis12345@users.noreply.github.com>
2022-10-04 14:48:29 +0700

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -5,7 +5,7 @@ let whmods = fs.readFileSync('./mp3/Bot.opus')
 conn.sendFile(m.chat, whmods, '', '', m, true)
 }
 
-handler.customPrefix = /^(bot|robot|p|tes|tes123|tes1|tes12|woy)$/i
+handler.customPrefix = /^(bot|halo|p|tes|klis|muklis|hello|woy)$/i
 handler.command = new RegExp
 
 export default handler
